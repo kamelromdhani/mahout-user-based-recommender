d427543c8871eec7e755927b16027618f253c32e
Kamel Romdhani <Kamel.Romdhani@devoteam.be>
2019-12-03 20:11:42 +0100

    getting data from mysql instead of csv file

--- src/main/java/com/example/MahoutUserBasedRecommenderApplication.java
+++ src/main/java/com/example/MahoutUserBasedRecommenderApplication.java
@@ -1,24 +1,21 @@
 package com.example;
 
-
-import java.io.File;
 import java.io.IOException;
 import java.util.List;
 
 import org.apache.mahout.cf.taste.common.TasteException;
-import org.apache.mahout.cf.taste.impl.common.FastIDSet;
-import org.apache.mahout.cf.taste.impl.common.LongPrimitiveIterator;
-import org.apache.mahout.cf.taste.impl.model.file.FileDataModel;
-import org.apache.mahout.cf.taste.impl.neighborhood.ThresholdUserNeighborhood;
+import org.apache.mahout.cf.taste.impl.model.jdbc.MySQLJDBCDataModel;
+import org.apache.mahout.cf.taste.impl.neighborhood.NearestNUserNeighborhood;
 import org.apache.mahout.cf.taste.impl.recommender.GenericUserBasedRecommender;
 import org.apache.mahout.cf.taste.impl.similarity.PearsonCorrelationSimilarity;
-import org.apache.mahout.cf.taste.model.DataModel;
+import org.apache.mahout.cf.taste.model.JDBCDataModel;
 import org.apache.mahout.cf.taste.neighborhood.UserNeighborhood;
 import org.apache.mahout.cf.taste.recommender.RecommendedItem;
-import org.apache.mahout.cf.taste.recommender.UserBasedRecommender;
 import org.apache.mahout.cf.taste.similarity.UserSimilarity;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.mysql.cj.jdbc.MysqlDataSource;
 
 @SpringBootApplication
 public class MahoutUserBasedRecommenderApplication {
@@ -28,27 +25,26 @@ public class MahoutUserBasedRecommenderApplication {
 		
 		try {
 		
-			DataModel model = new FileDataModel(new File("C:\\Users\\kromdhani\\Documents\\workspace-spring-tool-suite-4-4.4.0.RELEASE\\mahout-user-based-recommender\\src\\main\\resources\\dataset.csv"));
-			
-			
-			/*In this example, we want to create a user-based recommender. 
-			 * The idea behind this approach is that when we want to compute recommendations for a particular users, we look for other users with a similar taste and pick the recommendations from their items.
-			 * For finding similar users, we have to compare their interactions.
-			 * There are several methods for doing this. One popular method is to compute the correlation coefficient between their interactions.
-			 * The range of values for the correlation coefficient is -1.0 to 1.0
-			 * In Mahout, you use this method as follows: */
-			
-			UserSimilarity similarity = new PearsonCorrelationSimilarity(model);
-			UserNeighborhood neighborhood = new ThresholdUserNeighborhood(0.4, similarity, model); // correlation between 2 users equal 0.1
-			
-			UserBasedRecommender recommender = new GenericUserBasedRecommender(model, neighborhood, similarity);
-			
-			List<RecommendedItem> recommendations = recommender.recommend(2, 3); //get 3 items recommended for the user with userID 2
-			
-			for (RecommendedItem recommendation : recommendations) {
-			  System.out.println(recommendation);
-			  
-			}
+			MysqlDataSource dataSource = new MysqlDataSource();
+	        dataSource.setServerName("localhost");
+	        dataSource.setUser("root");
+	        dataSource.setPassword("root");
+	        dataSource.setDatabaseName("rec");
+	        
+	        JDBCDataModel dm = new MySQLJDBCDataModel(dataSource,"ratings","userid","itemid","rating","");
+	        
+	        UserSimilarity similarity = new PearsonCorrelationSimilarity(dm);
+	        UserNeighborhood neighbor = new NearestNUserNeighborhood(2,similarity, dm);
+	        
+	        GenericUserBasedRecommender recommender = new GenericUserBasedRecommender(dm, neighbor, similarity);
+	        List<RecommendedItem> list = recommender.recommend(2, 3);// recommend
+	                                                                 // one item
+	                                                                 // to user
+	                                                                 // 1
+	        
+	        for (RecommendedItem ri : list) {
+	            System.out.println(ri);
+	        }
 		}catch (Exception e) {
 			System.out.println(e.getMessage());
 		}
---
+++ src/main/resources/data/tables.sql
@@ -0,0 +1,42 @@
+# Tables creation 
+create table ratings ( 
+   userid int, 
+   itemid int, 
+   rating int, 
+   timestamp int, 
+   primary key (userid, itemid)); 
+
+create table items ( 
+   itemid int primary key, 
+   title text, 
+   videodate text, 
+   imdb text, 
+   action boolean, 
+   adventure boolean, 
+   animation boolean, 
+   childrens boolean, 
+   comedy boolean, 
+   crime boolean, 
+   documentary boolean, 
+   drama boolean, 
+   fantasy boolean, 
+   noir boolean, 
+   horror boolean, 
+   musical boolean, 
+   mystery boolean, 
+   romance boolean, 
+   scifi boolean, 
+   thriller boolean, 
+   war boolean, 
+   western boolean); 
+ 
+create table users ( 
+   userid int primary key, 
+   age int, 
+   gender char, 
+   occupation text, 
+   zip varchar); 
+ 
+# Indexes creation 
+create index usersdata_index on ratings (userid); 
+create index itemsdata_index on ratings (itemid);
